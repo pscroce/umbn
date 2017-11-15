a55bc3a82fb8ee3f50e9b078eb29ac54107677c8
pscroce <pscroce@gmail.com>
2017-11-15 14:50:17 -0500

    Wipe out previous lessons for a fresh slate.

--- lessons-learned/lessons.js
+++
@@ -1,7 +0,0 @@
-
-// https://gist.github.com/mnelson/32914b080c46f5104f8b
-//https://stackoverflow.com/questions/37478854/typeerror-react-rendertostaticmarkup-is-not-a-function
-// https://github.com/FormidableLabs/radium/tree/master/docs/api#keyframes
-// https://github.com/FormidableLabs/radium/tree/master/docs/api#styleroot-component
-// https://www.npmjs.com/package/radium-normalize
-//https://purecss.io/start/
