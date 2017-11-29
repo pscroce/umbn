65a7c7b99cff54d72fd8b5dcaeed0406e6da3043
pscroce <pscroce@gmail.com>
2017-11-29 11:14:47 -0500

    Move unused components to archive.

--- src/App.js
+++ src/App.js
@@ -2,15 +2,36 @@ import React, { Component } from 'react';
 import Radium, { StyleRoot } from 'radium';
 import Offline from 'react-offline';
 
-import AppContents from './components/layout/AppContents';
-import OnlineMessage from './components/layout/OnlineMessage';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch } from 'react-router-dom';
+
+import About from './components/pages/About';
+import Home from './components/pages/Home';
+import How from './components/pages/How';
+
+// Posts
+import PostOne from './components/pages/posts/PostOne';
+
+import './index.css';
 
 class App extends Component {
   render() {
     return (
       <StyleRoot>
         <div style={styles.app}>
-          <AppContents />
+          <Router>
+            <div>
+              <Switch>
+                <Route exact path='/' component={Home} />
+                <Route path='/about' component={About} />
+                <Route path='/how' component={How} />
+                <Route path='/posts/one' component={PostOne} />
+                <Route component={Home} />
+              </Switch>
+            </div>
+          </Router>
         </div>
       </StyleRoot>
     );
