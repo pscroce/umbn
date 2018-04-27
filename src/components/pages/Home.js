d5d583823eaa9dc4b3dfed36a9c002aa15d4a086
pscroce <pscroce@gmail.com>
2018-04-27 10:50:02 -0400

    Revert to former button styling.

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -12,7 +12,7 @@ function Home() {
   return (
     <Section>
       <div style={styles.titleArea}>
-        <Title text='Take a break.' style={styles.title} />
+        <Title text='Paying attn?' style={styles.title} />
       </div>
       <div style={styles.eyeArea}>
         <button style={[styles.leftSide, styles.buttonResets]} className='eye-link' id='open-popup'>
